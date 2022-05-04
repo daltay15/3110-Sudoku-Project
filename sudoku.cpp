51f736d6644ef1569401987bd4aab5ade9dbfcd2
Demir Altay <77849115+daltay15@users.noreply.github.com>
2022-05-04 16:43:36 -0500

    Potentially fixed bug in isSafe

--- sudoku.cpp
+++ sudoku.cpp
@@ -4,47 +4,47 @@ using namespace std;
 
 void sudoku::printGrid(int arr[SIZE][SIZE])
 {
-     for (int i = 0; i < SIZE; i++)
-     {
-          for (int j = 0; j < SIZE; j++)
-          {
-               cout << arr[i][j] << " "; // print out the sudoku grid
-          }
-          cout << endl;
-     }
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            cout << arr[i][j] << " "; // print out the sudoku grid
+        }
+        cout << endl;
+    }
 }
 
 bool sudoku::isSafe(int sudokuGrid[SIZE][SIZE], int row, int col, int num)
 {
-     for (int x = 0; x < SIZE - 1; x++)
-     {
-          if (sudokuGrid[row][x] == num) // check row for the same number
-          {
-               return false;
-          }
-     }
+    for (int x = 0; x < SIZE; x++)
+    {
+        if (sudokuGrid[row][x] == num) // check row for the same number
+        {
+            return false;
+        }
+    }
 
-     for (int x = 0; x < SIZE - 1; x++)
-     {
-          if (sudokuGrid[x][col] == num) // check column for the same number
-          {
-               return false;
-          }
-     }
+    for (int x = 0; x < SIZE; x++)
+    {
+        if (sudokuGrid[x][col] == num) // check column for the same number
+        {
+            return false;
+        }
+    }
 
-     int startRow = row - row % boxRowSize;
-     int startCol = col - col % boxColSize;
+    int startRow = row - row % boxRowHeight;
+    int startCol = col - col % boxColWidth;
 
-     for (int i = 0; i < boxRowSize; i++)
-          for (int j = 0; j < boxColSize; j++)
-          {
-               if (sudokuGrid[i + startRow][j + startCol] == num) // checks the respective box to see if the number already exists
-               {
-                    return false;
-               }
-          }
+    for (int i = 0; i < boxRowHeight; i++)
+        for (int j = 0; j < boxColWidth; j++)
+        {
+            if (sudokuGrid[i + startRow][j + startCol] == num) // checks the respective box to see if the number already exists
+            {
+                return false;
+            }
+        }
 
-     return true;
+    return true;
 }
 
 bool sudoku::attemptSolve(int sudokuGrid[SIZE][SIZE], int row, int col)
