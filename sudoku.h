0ba76945d3215fd505cdd72f1c84cc6a104700c0
Demir Altay <77849115+daltay15@users.noreply.github.com>
2022-05-04 20:25:37 -0500

    Added function to get box width and height for dynamic puzzle size.

--- sudoku.h
+++ sudoku.h
@@ -1,13 +1,13 @@
 using namespace std;
 
 #define SIZE 6
-const int boxRowHeight = 2; // sets the rows in a box to 2 for small sudoku puzzle
-const int boxColWidth = 3; // sets the columns in a box to 3 for small sudoku puzzle
 
 class sudoku
 {
 private:
 public:
+    int getBoxHeight();
+    int getBoxWidth();
     void printGrid(int arr[SIZE][SIZE]);
     bool isSafe(int sudokuGrid[SIZE][SIZE], int row, int col, int num);
     bool attemptSolve(int sudokuGrid[SIZE][SIZE], int row, int col);
