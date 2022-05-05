147d4fdef2dc290704c3a194863818ec9b97eebb
Demir Altay <77849115+daltay15@users.noreply.github.com>
2022-05-05 02:22:14 -0500

    added function for getInput

--- sudoku.h
+++ sudoku.h
@@ -8,6 +8,7 @@ private:
 public:
     int getBoxHeight();
     int getBoxWidth();
+    void getInput(int sudokuGrid[SIZE][SIZE]);
     void printGrid(int arr[SIZE][SIZE]);
     bool isSafe(int sudokuGrid[SIZE][SIZE], int row, int col, int num);
     bool attemptSolve(int sudokuGrid[SIZE][SIZE], int row, int col);
